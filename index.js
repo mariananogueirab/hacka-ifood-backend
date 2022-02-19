6ed4d6cd8dea6bbd6581b3b18a2c42dcfe42546d
mariananbp <mariananbp@gmail.com>
2022-02-19 11:30:20 -0300

    add routes in index

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const errorMiddleware = require('./middlewares/errorMidlleware');
+const user = require('./routes/user.router');
+const login = require('./routes/login.router');
 
-
-const {PORT} = process.env;
+const { PORT } = process.env;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/user', user);
+app.use('/login', login);
 
 app.use(errorMiddleware);
 
